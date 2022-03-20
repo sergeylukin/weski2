cffed8033df0bde0e4a9c06a758f5b8e300fb639
Sergey Lukin <contact@sergeylukin.com>
2022-03-20 12:22:52 +0200

    apply styling

--- weski/apps/front-website/src/app/app.tsx
+++ weski/apps/front-website/src/app/app.tsx
@@ -1,9 +1,23 @@
 import { Route } from 'react-router-dom';
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
 
 import { Home } from './home/home';
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
+
+const colors = {
+  brand: {
+    500: '#2a69ac',
+  },
+};
+
+const theme = extendTheme({ colors });
+
+const ui = (child: ReactJSXElement) => (
+  <ChakraProvider theme={theme}>{child}</ChakraProvider>
+);
 
 export function App() {
-  return <Route path="/" exact render={() => <Home />} />;
+  return <Route path="/" exact render={() => ui(<Home />)} />;
 }
 
 export default App;
